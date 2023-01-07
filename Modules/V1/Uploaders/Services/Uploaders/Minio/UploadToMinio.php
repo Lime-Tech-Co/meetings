8c8590b4d674a8a523cdf247d5e314a977efa3a3
Farshid Boroomand <farshidboroomand@gmail.com>
2023-01-07 19:50:46 +0330

    Uploader Service created and registered

---
+++ Modules/V1/Uploaders/Services/Uploaders/Minio/UploadToMinio.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\V1\Uploaders\Services\Uploaders\Minio;
+use Modules\V1\Uploaders\Services\Upload;
+
+class UploadToMinio extends Upload
+{
+    public function doUpload($file): string
+    {
+        return 'path url from Minio service';
+    }
+}
