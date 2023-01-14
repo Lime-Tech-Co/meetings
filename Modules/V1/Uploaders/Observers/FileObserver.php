f56b1907e4285068baa5c6867bb9854cb87ca057
Farshid Boroomand <farshidboroomand@gmail.com>
2023-01-14 10:50:46 +0330

    FileUploaded event raised

--- Modules/V1/Uploaders/Observers/FileObserver.php
+++ Modules/V1/Uploaders/Observers/FileObserver.php
@@ -2,17 +2,20 @@
 
 namespace Modules\V1\Uploaders\Observers;
 
-use Modules\V1\Meetings\Commands\Importer\EmployeeBusyTimeImporter;
+use Modules\V1\Uploaders\Models\File;
+use Modules\V1\Uploaders\Events\FileUploaded;
 
 class FileObserver
 {
     /**
      * Handle the File "created" event.
      *
+     * @param File $file
+     *
      * @return void
      */
-    public function created(): void
+    public function created(File $file): void
     {
-        \Artisan::call(EmployeeBusyTimeImporter::class);
+        event(new FileUploaded($file));
     }
 }
